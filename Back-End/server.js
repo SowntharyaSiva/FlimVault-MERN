fca5ac12d6f273336c3649d6514298eef5274ca3
SowntharyaSiva <162068310+SowntharyaSiva@users.noreply.github.com>
2025-05-24 22:40:40 +0530

    Update server.js

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const User = require('./models/User'); // Your Mongoose User model
+const User = require('./models/User'); 
 require('dotenv').config();
 
 const app = express();
