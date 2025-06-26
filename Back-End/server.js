51a21df60a7eac7572f9b64e1cf90fa087a2710d
SowntharyaSiva <sowntharyasiva14@gmail.com>
2025-06-26 12:14:26 +0530

    changing port

--- Back-End/server.js
+++ Back-End/server.js
@@ -81,4 +81,6 @@ app.post('/watchlist/delete', async (req, res) => {
   res.json(user.watchlist);
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
